6de7de58c99f7d13e469824d57f6a9a69c4fba8d
ianlxlc <77240540+ianlxlc@users.noreply.github.com>
2021-08-31 05:10:31 -0700

    Add files via upload

--- gogopi.py
+++ gogopi.py
@@ -120,27 +120,22 @@ try:
     # test partial refresh
     # epd.init(epd.PART_UPDATE)
     # testImage = Image.open(os.path.join(picdir, 'space_talk_s.jpg'))
-    # epd.displayPartial(epd.getbuffer(testImage))
+    # test_space_img_r = ref_r + 60
+    # test_space_img_d = ref_d + ref_width - 100
+    # draw.rectangle((test_space_img_r, test_space_img_d, test_space_img_r+63, test_space_img_d+56), fill = 255)
+    # image.paste(testImage,(test_space_img_r,test_space_img_d))
+    # epd.displayPartial(epd.getbuffer(image))
+
+
     epd.init(epd.PART_UPDATE)
-    testImage = Image.open(os.path.join(picdir, 'space_talk_s.jpg'))
-    test_space_img_r = ref_r + 60
-    test_space_img_d = ref_d + ref_width - 100
-    draw.rectangle((test_space_img_r, test_space_img_d, test_space_img_r+63, test_space_img_d+56), fill = 255)
-    image.paste(testImage,(test_space_img_r,test_space_img_d))
-    epd.displayPartial(epd.getbuffer(image))
-    epd.displayPartial(epd.getbuffer(image))
-    epd.displayPartial(epd.getbuffer(image))
-
-
-    # epd.init(epd.PART_UPDATE)
-    # num = 0
-    # while (True):
-    #     draw.rectangle((120, 80, 220, 105), fill = 255)
-    #     draw.text((120, 80), time.strftime('%H:%M:%S'), font = heading_type_1, fill = 0)
-    #     epd.displayPartial(epd.getbuffer(image))
-    #     num = num + 1
-    #     if(num == 10):
-    #         break
+    num = 0
+    while (True):
+        draw.rectangle((60, 70, 160, 95), fill = 255)
+        draw.text((60, 70), time.strftime('%H:%M:%S'), font = heading_type_1, fill = 0)
+        epd.displayPartial(epd.getbuffer(image))
+        num = num + 1
+        if(num == 20):
+            break
 
 
     
